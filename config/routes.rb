9a8b419cfcb0548064de0937ac87d63b4b993f65
Ubuntu <vagrant@Base-Box>
2018-11-30 03:09:03 +0000

    Created form of helper for cart to identify items

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :carts
   resources :hair_products
+
+  root 'hair_products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
