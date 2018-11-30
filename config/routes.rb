462b7efd993d4e9feacec219005e63b52d676b08
Ubuntu <vagrant@Base-Box>
2018-11-30 06:09:20 +0000

    So far I have added conditions for a user to sign in or out. I have also created and assigned an admin user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
+  resources :displays
+  devise_for :users
   resources :cart_items
   resources :hair_products
 
-  root 'hair_products#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
